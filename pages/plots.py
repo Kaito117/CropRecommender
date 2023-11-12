b728431417e0a18d17bb7cb1a3667cde11507251
Pranshu Raj <113211320+Kaito117@users.noreply.github.com>
2023-11-12 05:11:12 +0000

    changed file structure, added page for plots

---
+++ pages/plots.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.write('<h2>Plots to understand data distribution')
+
+st.image('static/feature_correlation.png')
+
+st.image('static/ph_vs_label.png')
+
+st.image('static/temperature_vs_labels.png')
+
+st.image('static/total_data.png')
