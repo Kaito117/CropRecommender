4bc06188699104aa7a149a7ef0ca5f350a6ce1e7
Kaito117 <pranshuraj65536@gmail.com>
2023-11-14 19:00:57 +0530

    separated analysis, modeling and inference, added a page to show metrics

---
+++ pages/modeling.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import pickle
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report, f1_score
+import warnings
+
+warnings.filterwarnings("ignore")
+
+
+seed = 41
+
+
+train_data = pd.read_csv("train_data_cleaned.csv")
+test_data = pd.read_csv("test_data_cleaned.csv")
+
+X_train = train_data.iloc[:, :8]
+y_train = train_data.iloc[:, 8:]
+
+X_test = test_data.iloc[:, :8]
+y_test = test_data.iloc[:, 8:]
+
+
+def print_metrics(classifier, clf_name, classification_report=False):
+    print(clf_name)
+    print(f"Accuracy : {accuracy_score(y_test,classifier.predict(X_test))}")
+    print(f1_score(y_test, classifier.predict(X_test), average="macro"))
+    print()
+    if classification_report:
+        print(classification_report(y_test, classifier.predict(X_test)))
+
+
+logreg_clf = LogisticRegression(random_state=seed)
+logreg_clf.fit(X_train, y_train)
+print_metrics(logreg_clf, "Logistic Regression")
+
+
+svc_clf = SVC(random_state=seed)
+svc_clf.fit(X_train, y_train)
+print_metrics(svc_clf, "Support Vector machine")
+
+
+gnb_clf = GaussianNB()
+gnb_clf.fit(X_train, y_train)
+print_metrics(gnb_clf, "Gaussian Naive Bayes")
+
+
+rf_clf = RandomForestClassifier(random_state=seed)
+rf_clf.fit(X_train, y_train)
+print_metrics(rf_clf, "Random Forest")
+
+
+def store_models(store=False):
+    if store:
+        with open("gnb.pkl", "wb") as file:
+            pickle.dump(gnb_clf, file)
+
+        with open("rf.pkl", "wb") as file:
+            pickle.dump(rf_clf, file)
+
+        with open("logreg.pkl", "wb") as file:
+            pickle.dump(logreg_clf, file)
---
+++ pages/show_metrics.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pickle
+import pandas as pd
+from sklearn.metrics import accuracy_score, f1_score
+
+train_data = pd.read_csv("train_data_cleaned.csv")
+test_data = pd.read_csv("test_data_cleaned.csv")
+
+X_train = train_data.iloc[:, :8]
+y_train = train_data.iloc[:, 8:]
+
+X_test = test_data.iloc[:, :8]
+y_test = test_data.iloc[:, 8:]
+
+
+rf_clf = pickle.load(open("models/rf.pkl", "rb"))
+gnb_clf = pickle.load(open("models/gnb.pkl", "rb"))
+logreg_clf = pickle.load(open("models/logreg.pkl", "rb"))
+
+
+st.write("<h1>Metrics for models used for inference</h1>", unsafe_allow_html=True)
+
+
+def show_metrics(model, model_name):
+    st.write(f"""<h2>{model_name}</h2>""", unsafe_allow_html=True)
+    st.write(f"Accuracy : {accuracy_score(y_test,model.predict(X_test))}")
+    st.write(f"F1 score : {f1_score(y_test,model.predict(X_test),average='macro')}\n")
+
+
+show_metrics(rf_clf, "Random Forest")
+show_metrics(gnb_clf, "Gaussian Naive Bayes")
+show_metrics(logreg_clf, "Logistic Regression")
